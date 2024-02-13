f6fd33e0e04898ef4ded18d1e9d3997eea9ffb2e
Bahar Fekri <b.fekri@rahkar.team>
2024-02-13 18:24:23 +0330

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Bahar on 11/24/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
