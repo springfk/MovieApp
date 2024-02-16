d6eb5f21e84becb0c9fd2e638f9f55fbe93f5ab5
Bahar Fekri <b.fekri@rahkar.team>
2024-02-16 17:10:46 +0330

    Restructure project dircetories

---
+++ MovieApp/Components/UI/MovieAppColors.swift
@@ -0,0 +1,8 @@
+//
+//  MovieAppColors.swift
+//  MovieApp
+//
+//  Created by Bahar on 11/26/1402 AP.
+//
+
+import Foundation
---
+++ MovieApp/Components/UI/SideAtributtedBars.swift
@@ -0,0 +1,8 @@
+//
+//  SideAtributtedBars.swift
+//  MovieApp
+//
+//  Created by Bahar on 11/26/1402 AP.
+//
+
+import Foundation
